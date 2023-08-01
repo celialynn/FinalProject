bc32561aeb1f4e9a1cb8aada621a351ab5484415
Celia Schreiber <schreibercelia@gmail.com>
2023-08-01 09:35:49 +0100

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
